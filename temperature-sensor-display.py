00db4deeca3cea6cd605549b1205a02bb29c9667
Risto Wieland <risto_wieland@yahoo.com>
2024-02-18 16:43:12 +0100

    working on auto updates

---
+++ temperature-sensor-display.py
@@ -0,0 +1,427 @@
+import os
+import glob
+import time
+import sys
+import subprocess
+sys.path.append('/home/kermit/bcm2835-1.71/1.44inch-LCD-HAT-Code/RaspberryPi/python')
+import LCD_1in44
+from PIL import Image,ImageDraw,ImageFont,ImageColor
+import smbus
+from datetime import datetime, timezone
+import psycopg2
+import socket
+
+# Config Register (R/W)
+_REG_CONFIG                 = 0x00
+# SHUNT VOLTAGE REGISTER (R)
+_REG_SHUNTVOLTAGE           = 0x01
+
+# BUS VOLTAGE REGISTER (R)
+_REG_BUSVOLTAGE             = 0x02
+
+# POWER REGISTER (R)
+_REG_POWER                  = 0x03
+
+# CURRENT REGISTER (R)
+_REG_CURRENT                = 0x04
+
+# CALIBRATION REGISTER (R/W)
+_REG_CALIBRATION            = 0x05
+
+class BusVoltageRange:
+    """Constants for ``bus_voltage_range``"""
+    RANGE_16V               = 0x00      # set bus voltage range to 16V
+    RANGE_32V               = 0x01      # set bus voltage range to 32V (default)
+
+class Gain:
+    """Constants for ``gain``"""
+    DIV_1_40MV              = 0x00      # shunt prog. gain set to  1, 40 mV range
+    DIV_2_80MV              = 0x01      # shunt prog. gain set to /2, 80 mV range
+    DIV_4_160MV             = 0x02      # shunt prog. gain set to /4, 160 mV range
+    DIV_8_320MV             = 0x03      # shunt prog. gain set to /8, 320 mV range
+
+class ADCResolution:
+    """Constants for ``bus_adc_resolution`` or ``shunt_adc_resolution``"""
+    ADCRES_9BIT_1S          = 0x00      #  9bit,   1 sample,     84us
+    ADCRES_10BIT_1S         = 0x01      # 10bit,   1 sample,    148us
+    ADCRES_11BIT_1S         = 0x02      # 11 bit,  1 sample,    276us
+    ADCRES_12BIT_1S         = 0x03      # 12 bit,  1 sample,    532us
+    ADCRES_12BIT_2S         = 0x09      # 12 bit,  2 samples,  1.06ms
+    ADCRES_12BIT_4S         = 0x0A      # 12 bit,  4 samples,  2.13ms
+    ADCRES_12BIT_8S         = 0x0B      # 12bit,   8 samples,  4.26ms
+    ADCRES_12BIT_16S        = 0x0C      # 12bit,  16 samples,  8.51ms
+    ADCRES_12BIT_32S        = 0x0D      # 12bit,  32 samples, 17.02ms
+    ADCRES_12BIT_64S        = 0x0E      # 12bit,  64 samples, 34.05ms
+    ADCRES_12BIT_128S       = 0x0F      # 12bit, 128 samples, 68.10ms
+
+class Mode:
+    """Constants for ``mode``"""
+    POWERDOW                = 0x00      # power down
+    SVOLT_TRIGGERED         = 0x01      # shunt voltage triggered
+    BVOLT_TRIGGERED         = 0x02      # bus voltage triggered
+    SANDBVOLT_TRIGGERED     = 0x03      # shunt and bus voltage triggered
+    ADCOFF                  = 0x04      # ADC off
+    SVOLT_CONTINUOUS        = 0x05      # shunt voltage continuous
+    BVOLT_CONTINUOUS        = 0x06      # bus voltage continuous
+    SANDBVOLT_CONTINUOUS    = 0x07      # shunt and bus voltage continuous
+
+
+class INA219:
+    def __init__(self, i2c_bus=1, addr=0x40):
+        self.bus = smbus.SMBus(i2c_bus);
+        self.addr = addr
+
+        # Set chip to known config values to start
+        self._cal_value = 0
+        self._current_lsb = 0
+        self._power_lsb = 0
+        self.set_calibration_16V_5A()
+
+    def read(self,address):
+        data = self.bus.read_i2c_block_data(self.addr, address, 2)
+        return ((data[0] * 256 ) + data[1])
+
+    def write(self,address,data):
+        temp = [0,0]
+        temp[1] = data & 0xFF
+        temp[0] =(data & 0xFF00) >> 8
+        self.bus.write_i2c_block_data(self.addr,address,temp)
+
+    def set_calibration_16V_5A(self):
+        """Configures to INA219 to be able to measure up to 16V and 5A of current. Counter
+           overflow occurs at 16A.
+           ..note :: These calculations assume a 0.01 shunt ohm resistor is present
+        """
+        # By default we use a pretty huge range for the input voltage,
+        # which probably isn't the most appropriate choice for system
+        # that don't use a lot of power.  But all of the calculations
+        # are shown below if you want to change the settings.  You will
+        # also need to change any relevant register settings, such as
+        # setting the VBUS_MAX to 16V instead of 32V, etc.
+
+        # VBUS_MAX = 16V             (Assumes 16V, can also be set to 32V)
+        # VSHUNT_MAX = 0.08          (Assumes Gain 2, 80mV, can also be 0.32, 0.16, 0.04)
+        # RSHUNT = 0.01               (Resistor value in ohms)
+
+        # 1. Determine max possible current
+        # MaxPossible_I = VSHUNT_MAX / RSHUNT
+        # MaxPossible_I = 8.0A
+
+        # 2. Determine max expected current
+        # MaxExpected_I = 5.0A
+
+        # 3. Calculate possible range of LSBs (Min = 15-bit, Max = 12-bit)
+        # MinimumLSB = MaxExpected_I/32767
+        # MinimumLSB = 0.0001529              (61uA per bit)
+        # MaximumLSB = MaxExpected_I/4096
+        # MaximumLSB = 0,0012207              (488uA per bit)
+
+        # 4. Choose an LSB between the min and max values
+        #    (Preferrably a roundish number close to MinLSB)
+        # CurrentLSB = 0.00016 (uA per bit)
+        self._current_lsb = 0.1524  # Current LSB = 100uA per bit
+
+        # 5. Compute the calibration register
+        # Cal = trunc (0.04096 / (Current_LSB * RSHUNT))
+        # Cal = 13434 (0x347a)
+
+        self._cal_value = 26868
+
+        # 6. Calculate the power LSB
+        # PowerLSB = 20 * CurrentLSB
+        # PowerLSB = 0.002 (2mW per bit)
+        self._power_lsb = 0.003048  # Power LSB = 2mW per bit
+
+        # 7. Compute the maximum current and shunt voltage values before overflow
+        #
+        # Max_Current = Current_LSB * 32767
+        # Max_Current = 3.2767A before overflow
+        #
+        # If Max_Current > Max_Possible_I then
+        #    Max_Current_Before_Overflow = MaxPossible_I
+        # Else
+        #    Max_Current_Before_Overflow = Max_Current
+        # End If
+        #
+        # Max_ShuntVoltage = Max_Current_Before_Overflow * RSHUNT
+        # Max_ShuntVoltage = 0.32V
+        #
+        # If Max_ShuntVoltage >= VSHUNT_MAX
+        #    Max_ShuntVoltage_Before_Overflow = VSHUNT_MAX
+        # Else
+        #    Max_ShuntVoltage_Before_Overflow = Max_ShuntVoltage
+        # End If
+
+        # 8. Compute the Maximum Power
+        # MaximumPower = Max_Current_Before_Overflow * VBUS_MAX
+        # MaximumPower = 3.2 * 32V
+        # MaximumPower = 102.4W
+
+        # Set Calibration register to 'Cal' calculated above
+        self.write(_REG_CALIBRATION,self._cal_value)
+
+        # Set Config register to take into account the settings above
+        self.bus_voltage_range = BusVoltageRange.RANGE_16V
+        self.gain = Gain.DIV_2_80MV
+        self.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.mode = Mode.SANDBVOLT_CONTINUOUS
+        self.config = self.bus_voltage_range << 13 | \
+                      self.gain << 11 | \
+                      self.bus_adc_resolution << 7 | \
+                      self.shunt_adc_resolution << 3 | \
+                      self.mode
+        self.write(_REG_CONFIG,self.config)
+
+    def getShuntVoltage_mV(self):
+        self.write(_REG_CALIBRATION,self._cal_value)
+        value = self.read(_REG_SHUNTVOLTAGE)
+        if value > 32767:
+            value -= 65535
+        return value * 0.01
+
+    def getBusVoltage_V(self):
+        self.write(_REG_CALIBRATION,self._cal_value)
+        self.read(_REG_BUSVOLTAGE)
+        return (self.read(_REG_BUSVOLTAGE) >> 3) * 0.004
+
+    def getCurrent_mA(self):
+        value = self.read(_REG_CURRENT)
+        if value > 32767:
+            value -= 65535
+        return value * self._current_lsb
+
+    def getPower_W(self):
+        self.write(_REG_CALIBRATION,self._cal_value)
+        value = self.read(_REG_POWER)
+        if value > 32767:
+            value -= 65535
+        return value * self._power_lsb
+
+def open_connection(db): 
+    global connection, cursor
+    db_params = {
+        "local": {
+            "user": "postgres",
+            "password": "mWHfAG_3Wv7gMo2B2UFC",
+            "host": "localhost",
+            "port": "5432",
+            "database": "portable_env"
+        },
+        "remote": {
+            "user": "postgres",
+            "password": "hTvLXAoamyHaMuds2cX8HDzperki",
+            "host": "139-162-151-203.ip.linodeusercontent.com",
+            "port": "5432",
+            "database": "portable_env"
+        }
+    }
+    if db in db_params:
+        params = db_params[db]
+        try:
+            connection = psycopg2.connect(
+                user=params["user"],
+                password=params["password"],
+                host=params["host"],
+                port=params["port"],
+                database=params["database"]
+            )
+            cursor = connection.cursor()
+#            print(f"Connection to {db} database successful")
+            return connection, cursor
+        except (Exception, psycopg2.Error) as error:
+#            print(f"Error while connecting to {db} database:", error)
+            return None, None
+    else:
+        print(f"Database {db} not found in parameters")
+        return None, None   
+
+
+def close_connection():
+    cursor.close()
+    connection.close()
+
+
+def create_table(db):
+    open_connection(db)
+    
+    try:
+        create_table_query = '''CREATE TABLE pfannenstiel(
+               timeStamp TIMESTAMP,
+               temperature REAL
+            ); '''       
+        
+        cursor.execute(create_table_query)
+        connection.commit()
+        close_connection()
+        
+    except Exception as err:
+        print ("Table pfannenstiel already exists")
+        close_connection()
+
+    else:
+        print("Table pfannenstiel created successfully in PostgreSQL")
+        close_connection()
+
+
+def drop_table():
+    open_connection()
+    
+    try:
+        cursor.execute("Drop table pfannenstiel;")
+        connection.commit()
+        close_connection()
+        
+    except (Exception, psycopg2.Error) as error:
+        print ("Table pfannenstiel does not exists")
+        print ("Error while connecting to PostgreSQL", error)
+        close_connection()
+
+    else:
+        print("Table pfannenstiel dropped successfully in PostgreSQL ")
+        close_connection()
+
+
+def insert_records(db, temperature):
+    open_connection(db)
+    try:
+        dt = datetime.now()
+        x = dt.replace(microsecond=0)  
+        postgres_insert_query = """ INSERT INTO pfannenstiel (timeStamp, temperature) VALUES (%s,%s)"""
+        record_to_insert = (x, temperature)
+        cursor.execute(postgres_insert_query, record_to_insert)
+        connection.commit()
+        close_connection()
+
+    except (Exception, psycopg2.Error) as error:
+        print ("Error while connecting to PostgreSQL", error)
+        close_connection()
+
+
+def check_network_connection():
+    try:
+        # Try to connect to a well-known external server
+        socket.create_connection(("8.8.8.8", 53), timeout=5)
+        return True
+    except OSError:
+        return False
+
+
+def move_records_to_remote_db():
+    open_connection("local")
+    try:
+        cursor.execute("SELECT * FROM pfannenstiel;")
+        records = cursor.fetchall()
+        close_connection()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while moving records to remote PostgreSQL", error)
+        close_connection()
+
+    open_connection("remote")
+    try:
+        for record in records:
+            print(record)
+            postgres_insert_query = """ INSERT INTO pfannenstiel (timeStamp, temperature) VALUES (%s,%s)"""
+            cursor.execute(postgres_insert_query, record)    
+        connection.commit()
+        close_connection()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while moving records to remote PostgreSQL", error)
+        close_connection()
+
+    open_connection("local")
+    try:
+        cursor.execute("DELETE FROM pfannenstiel;")
+        connection.commit()
+        close_connection()
+    except (Exception, psycopg2.Error) as error:
+        print("Error while moving records to remote PostgreSQL", error)
+        close_connection()
+
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = round(float(temp_string) / 1000.0, 1)
+        return temp_c
+
+LCD = LCD_1in44.LCD()
+Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
+LCD.LCD_Init(Lcd_ScanDir)
+LCD.LCD_Clear()
+
+# Load a font
+font_path = "/home/kermit/temperature_sensor/JMH Typewriter-Bold.ttf"
+font_size_1 = 18
+font_1 = ImageFont.truetype(font_path, font_size_1)
+font_size_2 = 44
+font_2 = ImageFont.truetype(font_path, font_size_2)
+
+# Create an INA219 instance.
+ina219 = INA219(addr=0x43)
+
+# drop_table()
+# create_table("remote")
+# create_table("local")
+
+while True:
+    # clear screen
+    image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+    draw = ImageDraw.Draw(image)
+    
+    # UPS Hat readings
+    bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
+    shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
+    current = ina219.getCurrent_mA()                   # current in mA
+    power = ina219.getPower_W()                        # power in W
+    p = (bus_voltage - 3)/1.2*100
+    if(p > 100):p = 100
+    if(p < 0):p = 0
+    battery = str(round(p,1))
+    # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
+
+    temperature = read_temp()
+
+    if check_network_connection():
+        insert_records("remote", temperature)
+        move_records_to_remote_db()
+    else:
+        insert_records("local", temperature)
+
+    temperature_str = str(temperature)
+    draw.text((5, 10), 'Temperatur: ', font=font_1, fill = "WHITE")
+    draw.text((5, 40), temperature_str+'°C ', font=font_2, fill = "WHITE")
+    draw.text((5, 100), 'Bat: '+battery+'%', font=font_1, fill = "WHITE")
+    LCD.LCD_ShowImage(image,0,0)
+    if LCD.digital_read(LCD.GPIO_KEY1_PIN) == 1 or p < 5: # Key1 is pressed or battery power is smaller than 5%
+       print ("System Shutdown")
+       image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+       draw = ImageDraw.Draw(image)
+       draw.text((5, 10), 'Sys', font=font_2, fill = "WHITE")
+       draw.text((5, 50), 'Shut', font=font_2, fill = "WHITE")
+       LCD.LCD_ShowImage(image,0,0)
+       time.sleep(5)
+       subprocess.run(["sudo", "shutdown", "-h", "now"])
+    time.sleep(10)
+
+
+
+
