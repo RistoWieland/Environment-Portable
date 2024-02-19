d058b179670be649b16fec854d63765663bc45fa
RistoWieland <risto_wieland@yahoo.com>
2024-02-19 15:45:10 +0100

    update

--- temperature-sensor-display.py
+++ temperature-sensor-display.py
@@ -402,8 +402,8 @@ def setting_menu(menu):
             image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
             draw = ImageDraw.Draw(image)
 
-            # Determine the range of items to display (max 5 items)
-            end_index = min(start_index + 5, num_items)
+            # Determine the range of items to display (max 6 items)
+            end_index = min(start_index + 6, num_items)
 
             # Display menu items
             for i, index in enumerate(range(start_index, end_index)):
@@ -413,7 +413,7 @@ def setting_menu(menu):
 
             # Display the selection rectangle
             selection_rect_y = 10 + (selected_index - start_index) * 20
-            draw.rectangle([(0, selection_rect_y), (LCD.width-1, selection_rect_y + 20)], outline="YELLOW")
+            draw.rectangle([(0, selection_rect_y), (LCD.width-1, selection_rect_y + 18)], outline="YELLOW")
 
             # Show the image on the LCD
             LCD.LCD_ShowImage(image, 0, 0)
