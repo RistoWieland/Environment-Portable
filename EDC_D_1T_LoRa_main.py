18a7d87acf9586f695c69e27aa71f8eab4691c2e
RistoWieland <risto_wieland@yahoo.com>
2024-03-03 13:04:29 +0100

    hello message

--- EDC_D_1T_LoRa_main.py
+++ EDC_D_1T_LoRa_main.py
@@ -308,7 +308,7 @@ font_4 = ImageFont.truetype(font_path, font_size_4)
 while True:
     image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
     draw = ImageDraw.Draw(image)
-    draw.text((5, 0), 'Temperatures: ', font=font_1, fill = "WHITE")
+    draw.text((5, 0), 'Hallo ', font=font_1, fill = "WHITE")
     LCD.LCD_ShowImage(image,0,0)
     received_message = node.receive()
     if received_message is not None:
