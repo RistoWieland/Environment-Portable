dddd7976407d444b5d3e9c8d774ce6021c336228
RistoWieland <risto_wieland@yahoo.com>
2024-03-04 10:05:17 +0100

    test if new updater script works

--- EDC_D_1T_LoRa_main.py
+++ EDC_D_1T_LoRa_main.py
@@ -38,8 +38,8 @@ global config_file
 config_file = '/home/statler/Config/config.ini'
 
 # here I keep track of which version this script is
-script_version = "v1.04"
-release_notes ="initial version. Check Network Connection Check"
+script_version = "v1.05"
+release_notes ="initial version. Check New Updater Script"
 
 
 def settings_reading(which_section, which_parameter):
